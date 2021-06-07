77c33a4f253263342f0718c719cc2908989b2474
V.Soroka <V.Soroka@x5.ru>
2021-06-07 23:21:53 +0300

    Add main menu

---
+++ Assets/Scripts/UI/LevelLoader.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+    public float transitionTime = 1.0f;
+
+    public void LoadLevel(string levelName)
+    {
+        StartCoroutine(StartLevel(levelName));
+    }
+
+    IEnumerator StartLevel(string levelName)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelName);
+    }
+}
