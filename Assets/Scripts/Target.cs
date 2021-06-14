fb21f1c6ca4f9657f954a3e9d1ea31cca073ab1e
V.Soroka <V.Soroka@x5.ru>
2021-06-15 00:15:36 +0300

    ADD HP

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -1,19 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Target : MonoBehaviour
 {
     public float health = 100f;
     public float maxHealth = 100f;
+    //private GameObject mainUIRef;
         
     public void TakeDamage(float amount)
     {
         health -= amount;
     }
+    //private void Start()
+    //{
+    //    mainUIRef = GameObject.FindGameObjectWithTag("MainUI");
+    //}
+
     // Update is called once per frame
     void Update()
     {
+        //mainUIRef.transform.Find("HPScrollbar").gameObject.GetComponent<Scrollbar>().value = health / maxHealth;
+        //mainUIRef.transform.Find("HPScrollbar").gameObject.GetComponent<Image>().color = Color.Lerp(Color.black, Color.red, health / maxHealth);
         if (health > maxHealth)
         {
             health = maxHealth;
