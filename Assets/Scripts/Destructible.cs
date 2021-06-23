ab518d3f5d16d5e8c76ff5fa140211f15f5b77e1
Alik <alik_okroyan@mail.ru>
2021-06-24 01:06:19 +0300

    improving destructible prefabs

--- Assets/Scripts/Destructible.cs
+++ Assets/Scripts/Destructible.cs
@@ -5,18 +5,51 @@ using UnityEngine;
 public class Destructible : MonoBehaviour
 {
     public GameObject destructiblePrefab;
-    private Target targetComponent;
-    // Start is called before the first frame update
-    void Start()
-    {
-        targetComponent = GetComponent<Target>();
-    }
 
-    // Update is called once per frame
+    private Dictionary<string, Vector3> partsPos = new Dictionary<string, Vector3>()
+    {
+
+    };
+    private Dictionary<string, Quaternion> partsRot = new Dictionary<string, Quaternion>()
+    {
+    };
+
+    private void Start()
+    {
+
+    }
     public void ChangeToDestructible()
     {
-        Instantiate(destructiblePrefab, transform.position, transform.rotation);
+
+        GameObject destructible = Instantiate(destructiblePrefab, transform.position, transform.rotation);
+
+        //// writing source parts
+        //foreach (Transform child in transform)
+        //{
+        //    if (child.name == "metarig") continue;
+
+        //    Debug.Log(child.name);
+
+        //    Vector3 pos = child.gameObject.GetComponent<SkinnedMeshRenderer>().rootBone.localPosition;
+        //    Quaternion rot = child.gameObject.GetComponent<SkinnedMeshRenderer>().rootBone.rotation;
+        //    if (pos != null)
+        //    {
+        //        partsPos.Add(child.name, transform.TransformPoint(pos));
+        //        partsRot.Add(child.name, rot);
+        //    }
+
+        //}
+        //// writing part pos and rot
+        //foreach (Transform child in destructible.transform)
+        //{
+        //    child.position = partsPos[child.name];
+        //    child.rotation = partsRot[child.name];
+
+        //    child.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+        //}
         Destroy(gameObject);
+
+
     }
 
 
--- Assets/Scripts/PieceDestroyer.cs
+++ Assets/Scripts/PieceDestroyer.cs
@@ -2,9 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DestroyTimer : MonoBehaviour
+public class PieceDestroyer : MonoBehaviour
 {
     public float timeToLive = Mathf.Infinity;
+    public float massAfterCollision = 0.001f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,9 +16,16 @@ public class DestroyTimer : MonoBehaviour
     void Update()
     {
         timeToLive -= Time.deltaTime;
+
+
         if (timeToLive <= 0f)
         {
             Destroy(gameObject);
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GetComponent<Rigidbody>().mass = massAfterCollision;
     }
 }
