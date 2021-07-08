6de2e47a1f16da26a8957fded8b405a7382f9d1d
GastarTestProjects <xe7272@gmail.com>
2021-07-08 03:43:15 +0300

    Joystick fix

--- Assets/Scripts/UI/Joystick.cs
+++ Assets/Scripts/UI/Joystick.cs
@@ -1,36 +1,26 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem.OnScreen;
 
-public class Joystick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler//, IDragHandler
+public class Joystick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
 {
     public RectTransform background = null;
-    //public RectTransform handle = null;
-    //public Vector2 input = Vector2.zero;
 
-    //private Vector2 joyPosition;
+    public OnScreenStick stick;
 
     public virtual void OnPointerDown(PointerEventData eventData)
     {
-        //background.gameObject.SetActive(true);
-        //OnDrag(eventData);
-        //joyPosition = eventData.position;
         background.position = eventData.position;
-        //handle.anchoredPosition = Vector2.zero; 
+        stick.OnPointerDown(eventData);
     }
 
-    //public void OnDrag(PointerEventData eventData)
-    //{
-    //    //Vector2 joyDirection = eventData.position - RectTransformUtility.WorldToScreenPoint(new Camera(), background.position);
-    //    //input = (joyDirection.magnitude > background.sizeDelta.x / 2f) ? joyDirection.normalized :
-    //    //    joyDirection / (background.sizeDelta.x / 2f);
-
-    //    //handle.anchoredPosition = (input * background.sizeDelta.x / 2f);
-    //}
+    public virtual void OnDrag(PointerEventData eventData)
+    {
+        stick.OnDrag(eventData);
+    }
 
     public virtual void OnPointerUp(PointerEventData eventData)
     {
-        //background.gameObject.SetActive(false);
-        //input = Vector2.zero;
-        //handle.anchoredPosition = Vector2.zero;
+        stick.OnPointerUp(eventData);
     }
 }
