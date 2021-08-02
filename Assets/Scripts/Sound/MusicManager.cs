d183a46a271d66c51a712281662b26936eec7361
GodDoesPlayDice <alik_okroyan@mail.ru>
2021-08-02 13:51:48 +0300

    simple music manager and some random music

---
+++ Assets/Scripts/Sound/MusicManager.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MusicState
+{
+    playing,
+    stop,
+    pause
+} 
+
+public class MusicManager : MonoBehaviour
+{
+
+    public AudioClip[] music;
+
+    private MusicState state = MusicState.stop;
+    private int currentTrackIndex = 0;
+    private AudioSource audioSource;
+
+    private void Update()
+    {
+        // play next track
+        currentTrackIndex++;
+        if (!audioSource.isPlaying &&  state == MusicState.playing)
+        {
+            currentTrackIndex++;
+            if (currentTrackIndex > music.Length - 1)
+            {
+                currentTrackIndex = 0;
+            }
+            audioSource.clip = music[currentTrackIndex];  
+        }   
+    }
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+        // initial settings
+        audioSource.loop = false;
+        audioSource.playOnAwake = false;
+
+        PlayTrackByIndex(0);
+    }
+
+    public void PlayNextTrack()
+    {
+
+    }
+
+    public void PlayTrackByName(string name)
+    {
+        AudioClip track = null;
+        for (int i = 0; i < music.Length; i++)
+        {
+            if (music[i].name == name)
+            {
+                track = music[i];
+                currentTrackIndex = i;
+                break;
+            }
+        }
+
+        if (track == null)
+        {
+            Debug.Log("Track with this name was not found");
+            return;
+        }
+        else if (track != null)
+        {
+            Debug.Log("Playing music track: " + track.name);
+
+            audioSource.clip = track;
+            audioSource.Play();
+            state = MusicState.playing;
+        }
+    }
+
+    public void PlayTrackByIndex(int index)
+    {
+        audioSource.clip = music[index];
+        audioSource.Play();
+        state = MusicState.playing;
+    }
+
+    public void Pause()
+    {
+        audioSource.Pause();
+        state = MusicState.pause;
+    }
+
+    public void Stop()
+    {
+        audioSource.Stop();
+        state = MusicState.stop;
+    }
+
+    public string GetCurrentTrackName()
+    {
+        if (audioSource.clip == null) return null;
+        return audioSource.clip.name;
+    }
+}
