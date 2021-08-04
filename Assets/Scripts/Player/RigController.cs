0710c43c8bdb0e5c53172f1074f37962fbbdde8c
GastarTestProjects <xe7272@gmail.com>
2021-08-04 17:13:59 +0300

    Add auto aim

--- Assets/Scripts/Player/RigController.cs
+++ Assets/Scripts/Player/RigController.cs
@@ -6,6 +6,9 @@ public class RigController : MonoBehaviour
 {
     public GameObject player;
     public float minLookAtDistance = 1f;
+    public float autoAimAngle = 15f;
+    public float maxAutoAimDistance = 25f;
+    public GameObject enemiesContainer;
 
     private PlayerController playerController;
 
@@ -17,13 +20,46 @@ public class RigController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 finalAimPoint = playerController.aimAtPosition;
+        Vector3 finalAimPoint = AutoAim(playerController.aimAtPosition);
         Vector3 playerPos = new Vector3(player.transform.position.x, finalAimPoint.y, player.transform.position.z);
         if (Vector3.Distance(finalAimPoint, playerPos) < minLookAtDistance)
         {
             Vector3 dir = finalAimPoint - playerPos;
-            finalAimPoint = Vector3.Normalize(dir) * minLookAtDistance;
+            finalAimPoint = playerPos + Vector3.Normalize(dir) * minLookAtDistance;
         }
         transform.position = new Vector3(finalAimPoint.x, 1.57f, finalAimPoint.z);
+    }
+
+    private Vector3 AutoAim(Vector3 aimPoint)
+    {
+        Vector3 aimFloorPosition = new Vector3(aimPoint.x, 0f, aimPoint.z);
+        var enemy = GetEnemyAimTo(aimFloorPosition - player.transform.position);
+        return enemy == null ? aimPoint : enemy.position;
+    }
+
+    private Transform GetEnemyAimTo(Vector3 aimVector)
+    {
+        Transform currentAimEnemy = null;
+        float currentAngle = 180f;
+        var enemiesTransform = enemiesContainer.transform;
+        for (int i = 0; i < enemiesTransform.childCount; i++)
+        {
+            var child = enemiesTransform.GetChild(i);
+            Vector3 toEnemy = child.transform.position - player.transform.position;
+            float angle = Mathf.Abs(Vector3.Angle(aimVector, toEnemy));
+            RaycastHit hitInfo;
+            bool isHit = Physics.Linecast(player.transform.position, child.position, out hitInfo, LayerMask.GetMask("Obstacle"));
+            if (angle < autoAimAngle
+                && angle < currentAngle
+                && Vector3.Distance(player.transform.position, child.position) < maxAutoAimDistance
+                && child.gameObject.GetComponentInChildren<Renderer>().isVisible
+                && !isHit)
+            {
+                currentAimEnemy = child;
+                currentAngle = angle;
+            }
+        }
+
+        return currentAimEnemy;
     }
 }
