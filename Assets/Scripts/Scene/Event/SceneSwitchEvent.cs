c7e169fcec71fff239fbc75ceb5f164510ec9577
GastarTestProjects <xe7272@gmail.com>
2021-08-17 16:32:54 +0300

    SceneSwitchEventSO

--- Assets/Scripts/Scene/Event/SceneSwitchEvent.cs
+++ Assets/Scripts/Scene/Event/SceneSwitchEvent.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "Custom/ChangeSceneEvent", order = 56)]
+[CreateAssetMenu(menuName = "Custom/SceneSwitchEvent", order = 56)]
 public class SceneSwitchEvent : GameEventWithParam<SceneSwitchEventParam>
 {
     
