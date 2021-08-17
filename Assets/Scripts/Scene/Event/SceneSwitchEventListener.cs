df9344cf5079887d6eac692b8719f60f098d19c2
GastarTestProjects <xe7272@gmail.com>
2021-08-17 16:04:17 +0300

    Fix

--- Assets/Scripts/Scene/Event/SceneSwitchEventListener.cs
+++ Assets/Scripts/Scene/Event/SceneSwitchEventListener.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SceneSwitchEventListener : GameEventWithParamListener<LoadProgressSceneEP>
+public class SceneSwitchEventListener : GameEventWithParamListener<SceneSwitchEventParam>
 {
     
 }
