8ba7e2fa3ebc6074ba8227623ce85e94e95f5a71
GastarTestProjects <xe7272@gmail.com>
2021-08-17 17:31:04 +0300

    Fix

--- Assets/Scripts/Scene/Event/MenuToggleEventListener.cs
+++ Assets/Scripts/Scene/Event/MenuToggleEventListener.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MenuToggleEventListener : GameEventWithParamListener<SceneSwitchEventParam>
+public class MenuToggleEventListener : GameEventWithParamListener<MenuToggleEventParam>
 {
     
 }
