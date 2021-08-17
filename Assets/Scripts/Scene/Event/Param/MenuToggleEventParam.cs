eb83639edb50596abcfb250735759ebf10624c11
GastarTestProjects <xe7272@gmail.com>
2021-08-17 17:06:16 +0300

    MneuToggleEvent

---
+++ Assets/Scripts/Scene/Event/Param/MenuToggleEventParam.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuToggleEventParam
+{
+    public bool showMenu;
+
+    public MenuToggleEventParam(bool showMenu)
+    {
+        this.showMenu = showMenu;
+    }
+}
