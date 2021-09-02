8ded458a717d7dbaa9629dbe7b7197b3c077fae3
GastarTestProjects <xe7272@gmail.com>
2021-09-02 15:17:02 +0300

    Add separate death screen

---
+++ Assets/UI/Scripts/DeathScreen.cs
@@ -0,0 +1,117 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class DeathScreen : MonoBehaviour
+{
+
+    public UIDocument pauseDoc;
+    public ProgressionHolder progressionHolder;
+    public ScoreCounter scoreCounter;
+    public IngameProgressionManager progressionManager;
+    public AudioSource audioSource;
+
+    private VisualElement rootEl;
+    private VisualElement expContainer;
+    private Label expLabelEl;
+    private Button newRunEl;
+    private Button mainMenuEl;
+
+    private Action newRunAction;
+    private Action mainMenuAction;
+
+    private Label scoreLabel;
+
+    private bool animationPlayed;
+
+    void OnEnable()
+    {
+        rootEl = pauseDoc.rootVisualElement;
+        expContainer = rootEl.Q("ExpContainer");
+        expLabelEl = expContainer.Q<Label>("ExpLabel");
+        newRunEl = rootEl.Q<Button>("NewRun");
+        mainMenuEl = rootEl.Q<Button>("MainMenu");
+
+        newRunEl.RegisterCallback<ClickEvent>(e => newRunAction?.Invoke());
+        mainMenuEl.RegisterCallback<ClickEvent>(e => mainMenuAction?.Invoke());
+
+        scoreLabel = rootEl.Q<Label>("ScoreLabel");
+        SetScore();
+        ChangeDeathScreen();
+    }
+
+    private void ChangeDeathScreen()
+    {
+        if (animationPlayed)
+        {
+            expLabelEl.text = progressionHolder.exp.ToString();
+        }
+        else
+        {
+            expLabelEl.text = progressionManager.startExp.ToString();
+        }
+
+        if (!animationPlayed && this.enabled)
+        {
+            StartExpAnimation();
+        }
+    }
+
+    private void StartExpAnimation()
+    {
+        animationPlayed = true;
+        StartCoroutine(ExpAnimationCoroutine());
+    }
+
+    private IEnumerator ExpAnimationCoroutine()
+    {
+        yield return new WaitForSecondsRealtime(0.6f);
+        int expLeft = scoreCounter.currentScore;
+        int expCurrent = progressionManager.startExp;
+        while (expLeft > 0)
+        {
+            int expDelta = Mathf.Min(100 + 100 * Mathf.FloorToInt(expLeft / 10000), expLeft);
+            expLeft -= expDelta;
+            expCurrent += expDelta;
+            scoreLabel.text = expLeft.ToString();
+            expLabelEl.text = expCurrent.ToString();
+            audioSource.Play();
+
+            float deltaTime = 0.03f + Mathf.Min(40f / (float)expLeft, 0.2f);
+            yield return new WaitForSecondsRealtime(deltaTime);
+        }
+    }
+
+    public void SetNewRunAction(Action newRunAction)
+    {
+        this.newRunAction = newRunAction;
+    }
+
+    public void SetMainMenuAction(Action mainMenuAction)
+    {
+        this.mainMenuAction = mainMenuAction;
+    }
+
+    private void SetScore()
+    {
+        if (animationPlayed)
+        {
+            scoreLabel.text = "0";
+            return;
+        }
+        scoreLabel.text = scoreCounter.currentScore.ToString();
+        if (scoreCounter.currentScore > progressionHolder.topScore)
+        {
+            progressionHolder.topScore = scoreCounter.currentScore;
+        }
+
+        if (scoreCounter.currentScore > progressionManager.startTopScore) {
+            rootEl.Q<Label>("RecordLabel").style.display = DisplayStyle.Flex;
+        } else
+        {
+            rootEl.Q<Label>("RecordLabel").style.display = DisplayStyle.None;
+        }
+    }
+}
--- Assets/UI/Scripts/PauseScreen.cs
+++ Assets/UI/Scripts/PauseScreen.cs
@@ -10,12 +10,8 @@ public class PauseScreen : MonoBehaviour
     public UIDocument pauseDoc;
     public ProgressionHolder progressionHolder;
     public ScoreCounter scoreCounter;
-    public IngameProgressionManager progressionManager;
-    public AudioSource audioSource;
 
     private VisualElement rootEl;
-    private VisualElement expContainer;
-    private Label expLabelEl;
     private Button resumeEl;
     private Button settingsEl;
     private Button newRunEl;
@@ -29,15 +25,9 @@ public class PauseScreen : MonoBehaviour
     private Label scoreLabel;
     private Label topScoreLabel;
 
-    private bool dead;
-
-    private bool animationPlayed;
-
     void OnEnable()
     {
         rootEl = pauseDoc.rootVisualElement;
-        expContainer = rootEl.Q("ExpContainer");
-        expLabelEl = expContainer.Q<Label>("ExpLabel");
         resumeEl = rootEl.Q<Button>("Resume");
         settingsEl = rootEl.Q<Button>("Settings");
         newRunEl = rootEl.Q<Button>("NewRun");
@@ -50,62 +40,7 @@ public class PauseScreen : MonoBehaviour
 
         scoreLabel = rootEl.Q<Label>("ScoreLabel");
         topScoreLabel = rootEl.Q<Label>("TopScoreLabel");
-        ChangeDeathScreen();
         SetScore();
-    }
-
-    public void SetDead(bool dead)
-    {
-        this.dead = dead;
-        ChangeDeathScreen();
-    }
-
-    private void ChangeDeathScreen()
-    {
-        if (expContainer != null) {
-            expContainer.style.display = dead ? DisplayStyle.Flex : DisplayStyle.None;
-            resumeEl.style.display = dead ? DisplayStyle.None : DisplayStyle.Flex;
-            settingsEl.style.display = dead ? DisplayStyle.None : DisplayStyle.Flex;
-
-            if (animationPlayed)
-            {
-                expLabelEl.text = progressionHolder.exp.ToString();
-            } else
-            {
-                expLabelEl.text = progressionManager.startExp.ToString();
-            }
-
-            if (dead && !animationPlayed && this.enabled)
-            {
-                Debug.Log("Animation played");
-                StartExpAnimation();
-            }
-        }
-    }
-
-    private void StartExpAnimation()
-    {
-        animationPlayed = true;
-        StartCoroutine(ExpAnimationCoroutine());
-    }
-
-    private IEnumerator ExpAnimationCoroutine()
-    {
-        yield return new WaitForSecondsRealtime(0.6f);
-        int expLeft = scoreCounter.currentScore;
-        int expCurrent = progressionManager.startExp;
-        while (expLeft > 0)
-        {
-            int expDelta = Mathf.Min(100 + 100 * Mathf.FloorToInt(expLeft / 10000), expLeft);
-            expLeft -= expDelta;
-            expCurrent += expDelta;
-            scoreLabel.text = expLeft.ToString();
-            expLabelEl.text = expCurrent.ToString();
-            audioSource.Play();
-
-            float deltaTime = 0.03f + Mathf.Min(40f / (float) expLeft, 0.2f);
-            yield return new WaitForSecondsRealtime(deltaTime);
-        }
     }
 
     public void SetSettingsAction(Action settingsAction)
@@ -130,11 +65,6 @@ public class PauseScreen : MonoBehaviour
 
     private void SetScore()
     {
-        if (animationPlayed)
-        {
-            scoreLabel.text = "0";
-            return;
-        }
         scoreLabel.text = scoreCounter.currentScore.ToString();
         if (scoreCounter.currentScore > progressionHolder.topScore)
         {
