bd004455328cd19fcd7bb49c21cfadae06bdd1bb
GodDoesPlayDice <alik_okroyan@mail.ru>
2021-09-06 15:30:13 +0300

    lava sound script

---
+++ Assets/Scripts/Environment/LavaSoundManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaSoundManager : MonoBehaviour
+{
+    private AudioSource audioSource;
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.Play();
+    }
+    public void AdjustVolume(float volume)
+    {
+        audioSource.volume = volume;
+    }
+
+}
