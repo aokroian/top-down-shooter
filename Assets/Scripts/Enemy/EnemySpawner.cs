a70774d69cfb731fb6d6d2964fe896eacb154dfe
GastarTestProjects <xe7272@gmail.com>
2021-09-07 18:37:05 +0300

    Balancing

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -172,7 +172,10 @@ public class EnemySpawner : MonoBehaviour
     {
         //int cost = Mathf.Min(costLeft, Random.Range(minEnemyCost, maxEnemyCost + 1));
         int cost = Random.Range(minEnemyCost, Mathf.Min(costLeft, maxEnemyCost));
-        Debug.Log("Cost: " + cost + "; min: " + minEnemyCost + "; max: " + maxEnemyCost + "; left: " + costLeft);
+        if (cost == 1 && costLeft > 1) {
+            cost = Random.Range(minEnemyCost, Mathf.Min(costLeft, maxEnemyCost));
+        }
+        //Debug.Log("Cost: " + cost + "; min: " + minEnemyCost + "; max: " + maxEnemyCost + "; left: " + costLeft);
         GameObject result = enemyCosts[1][0];
         for (int i = cost; i > 0; i --)
         {
