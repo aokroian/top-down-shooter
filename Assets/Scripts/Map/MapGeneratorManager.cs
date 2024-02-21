90f29da9fce496c7b321275afe7a2a3937d04a31
GastarTestProjects <xe7272@gmail.com>
2024-02-21 15:16:47 +0400

    Converted to new unity version

--- Assets/Scripts/Map/MapGeneratorManager.cs
+++ Assets/Scripts/Map/MapGeneratorManager.cs
@@ -1,9 +1,8 @@
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.AI.Navigation;
 using Unity.Jobs;
 using UnityEngine;
-using UnityEngine.AI;
 
 public class MapGeneratorManager : MonoBehaviour
 {
